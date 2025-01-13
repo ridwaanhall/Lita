8d557525c92bfcb2be1c3f986bd96051a19dc6de
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-13 21:03:42 +0700

    Refactor BaseView to use APIView and enhance response data structure; add GitHub Actions workflow to update last_updated field automatically

--- LITA/views.py
+++ LITA/views.py
@@ -1,10 +1,20 @@
-from django.http import JsonResponse
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class BaseView(View):
+class BaseView(APIView):
     def get(self, request, *args, **kwargs):
         data = {
-            'api': '/api/',
-            'web': '/web/',
+            "version": "1.0.0",
+            "author": {
+                "name": "ridwaanhall",
+                "email": "ridwaanhall.dev@gmail.com",
+                "website": "https://ridwaanhall.me",
+            },
+            "endpoints": {
+                "api": "/api/",
+                "web": "/web/",
+            },
+            "status": "active",
+            "last_updated": "2025-01-13T20:57:00Z"
         }
-        return JsonResponse(data)
+        return Response(data)
