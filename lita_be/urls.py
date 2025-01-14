e9f574ba260449a015cd14bdda65cc3be0bea0f1
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-14 11:12:10 +0700

    Add new API endpoints for player gifts, skills, and moments

--- lita_be/urls.py
+++ lita_be/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.JsonResponseView.as_view(), name='be_json_response'),
+    
     path('skill/', views.SkillAPIView.as_view(), name='be_skill'),
     path('kolZone/list/', views.KolZoneListAPIView.as_view(), name='kolzone-list-api'),
     path('player/inskills/', views.PlayerInSkillsAPIView.as_view(), name='player-inskills-api'),
@@ -11,4 +12,10 @@ urlpatterns = [
     path('player/inskill/batch/', views.PlayerInskillBatchAPIView.as_view(), name='player-inskill-batch-api'),
     
     path('player/detail/g3/', views.PlayerDetailAPIView.as_view(), name='player-detail-api'),
+    path('player/received/gift/', views.PlayerReceivedGiftAPIView.as_view(), name='player-received-gift-api'),
+    path('player/received/rank/gift/', views.PlayerReceivedRankGiftAPIView.as_view(), name='player-received-rank-gift-api'),
+    path('player/received/rank/total/', views.PlayerReceivedRankTotalAPIView.as_view(), name='player-received-rank-total-api'),
+    path('player/received/rank/order/', views.PlayerReceivedRankOrderAPIView.as_view(), name='player-received-rank-order-api'),
+    path('player/skill/comment/', views.PlayerSkillCommentAPIView.as_view(), name='player-skill-comment-api'),
+    path('moment/user/list/', views.MomentUserListAPIView.as_view(), name='moment-user-list-api'),
 ]
